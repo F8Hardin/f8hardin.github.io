aea6c7e54492b7c47ab1890225fa2ee15e6bc386
Fate Hardin <laf8hardin@gmail.com>
2025-07-11 15:31:17 -0500

    More page formatting. Starting the setup of the projects, about me, and contact sections.

--- src/App.js
+++ src/App.js
@@ -2,23 +2,45 @@ import './App.css';
 import RainbowDiv from './rainbowDiv';
 
 function App() {
+
+  function scrollToSection(sectionId) {
+    const el = document.getElementById(sectionId);
+    if (el) {
+      el.scrollIntoView({ behavior: "smooth" });
+    }
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <div style={{ height: "10%"}}><RainbowDiv></RainbowDiv></div>
         <div className="Title-bar">
           <div className='Title-area'>Fate's Portfolio</div>
-          <div className="Navigation-area">
-            <button className="Navigation-button">About Me</button>
-            <button className="Navigation-button">Projects</button>
-            <button className="Navigation-button">Contact</button>
+          <div className="Navigation-button-area">
+            <div className="Vertical-line"></div>
+            <button onClick={() => scrollToSection('AboutMe')} className="Navigation-button">About Me</button>
+            <div className="Vertical-line"></div>
+            <button onClick={() => scrollToSection('Projects')} className="Navigation-button">Projects</button>
+            <div className="Vertical-line"></div>
+            <button onClick={() => scrollToSection('Contact')} className="Navigation-button">Contact</button>
+            <div className="Vertical-line"></div>
           </div>
         </div>
         <div style={{ height: "10%"}}><RainbowDiv></RainbowDiv></div>
       </header>
       <div className='App-background-window'>
-        <div className="App-main-window">
-          Project Details coming soon. This site is a work in progress.
+        <div className="Introduction-section">
+        </div>
+        <div className="Navigation-section">
+          <div id="AboutMe" className="Navigable-window" >
+            About Me.
+          </div>
+          <div id="Projects" className="Navigable-window" >
+            Projects
+          </div>
+          <div id="Contact" className="Navigable-window" >
+            Contact
+          </div>
         </div>
       </div>
     </div>
